b958b3f69379ef390163f90035363bb9ca2776a7
mococa <xfazxtudo@gmail.com>
2021-06-06 06:09:43 +0000

    Added a footer; fixed css bugs (margin, width) and credits

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,29 @@ import React, {useState, useEffect} from 'react';
 import PokemonRow from './components/PokemonRow';
 import Pokemon from './components/Pokemon';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './styles/default.css';
+import './styles/mobile.css';
 //<Pokemon pokedata={currentPokemon}/>
 function App() {
     
     const [page, setPage] = useState(0)
     const [pokemons, setPokemons] = useState([]);
     const [currentPokemon, setCurrentPokemon] = useState(null)
-    const [pokeVisible, setPokeVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [list, showList] = useState(true)
 
+    const itemsPerPage = 10;
+    const pokeCount = 1118; // As it says in https://pokeapi.co/api/v2/pokemon
+
+
+
     async function fetchPokemonNames(){
-        const api_url = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page*10}`
+        setLoading(true);
+        const api_url = `https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${page*itemsPerPage}`
         const response = await fetch(api_url)
         const data = await response.json()
+        setLoading(false);
         setPokemons(data.results)
     }
     async function showPoke(pokemon){
@@ -36,7 +45,7 @@ function App() {
     return (
         <>
         <Header/>
-        <main>
+        <main className="main">
             {
                 list?
                     <>
@@ -46,9 +55,10 @@ function App() {
                             }
                         </div>
                         <div className="page-btns">
-                            {page?<button onClick={()=>paginate(-1)}>Prev</button>:null}
-                            <button onClick={()=>paginate(1)}>Next</button>
+                            {page?<button disabled={loading?true:""} onClick={()=>paginate(-1)}>Prev</button>:null}
+                            <button disabled={loading?true:""} onClick={()=>paginate(1)}style={{marginLeft: 'auto'}}>Next</button>
                         </div>
+                        <span style={{marginBottom:'30px'}}>Page {page+1}/{Math.ceil(pokeCount/itemsPerPage)}</span>
                     </> :
                     <>
                         <span onClick={()=>showList(true)}className="back-btn">Voltar</span>
@@ -57,6 +67,7 @@ function App() {
             }
             
         </main>
+        <Footer/>
         </>
     );
 }
